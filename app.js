d4fe4ebcf32d5ffa339fee44945ca9121b4d7ae8
theob0t <theo06bot75@gmail.com>
2019-03-18 18:10:34 +0100

    Connect to database + queries

---
+++ app.js
@@ -0,0 +1,73 @@
+const imdb = require('./src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://main:esilv2015@denzeldb-luw1i.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "DenzelDB";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(9292, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("movies");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.post("/movies/populate", async(request, response) => {
+	const movies = await imdb(DENZEL_IMDB_ID);
+	collection.insertMany(movies, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+	});
+	console.log("The database has been populated.")
+});
+
+app.get("/movies", (request, response) => {
+	collection.aggregate([{ $match: { "metascore": {$gt:70}}}, { $sample: { size: 1 }}]).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+    console.log("Query done.");
+});
+
+app.get("/movies/:id", (request, response) => {
+  collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+    if(error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
+  console.log("Update done.")
+});
+
+app.get("/movies/search", (request, response) => {
+	var limit = 5, metascore = 0;
+	if(request.query.limit != undefined) limit = request.query.limit;
+	if(request.query.metascore != undefined) metascore = request.query.metascore;
+	
+	collection.aggregate([{$match:{"metascore": {$gte:Number(metascore)}}}, {$limit:Number(limit)}, {$sort:{"metascore":-1}}]).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+    console.log("Search done.");
+});
+
--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,44 @@
 /* eslint-disable no-console, no-process-exit */
 const imdb = require('./src/imdb');
+const assert = require('assert');
 const DENZEL_IMDB_ID = 'nm0000243';
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 
-async function sandbox (actor) {
-  try {
-    console.log(`📽️  fetching filmography of ${actor}...`);
-    const movies = await imdb(actor);
-    const awesome = movies.filter(movie => movie.metascore >= 77);
+const CONNECTION_URL = "mongodb+srv://main:esilv2015@denzeldb-luw1i.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "DenzelDB";
+const client = new MongoClient(CONNECTION_URL, { useNewUrlParser: true });
 
-    console.log(`🍿 ${movies.length} movies found.`);
-    console.log(JSON.stringify(movies, null, 2));
-    console.log(`🥇 ${awesome.length} awesome movies found.`);
-    console.log(JSON.stringify(awesome, null, 2));
-    process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+
+var database, collection;
+
+
+async function main()
+{
+    const movies = await imdb(DENZEL_IMDB_ID);
+    const dbName = 'movies';
+
+    client.connect(function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        const db = client.db(dbName);
+
+        db.collection("films").insertMany(movies, null, function (error, results) {
+            if (error) throw error;
+
+            console.log("Le document a bien été inséré"); 
+
+            client.close();
+
+        });
+    });
+
 }
 
-sandbox(DENZEL_IMDB_ID);
+
+main();
+
+
